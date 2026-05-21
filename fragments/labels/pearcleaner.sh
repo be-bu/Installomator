86cfcd0e817b1e219e68b28b3fb779662eac07f7
be-bu <98894746+be-bu@users.noreply.github.com>
2026-05-21 15:07:05 +0200

    Add pearcleaner.sh for Pearcleaner app metadata

---
+++ fragments/labels/pearcleaner.sh
@@ -0,0 +1,7 @@
+pearcleaner)
+    name="Pearcleaner"
+    type="zip"
+    downloadURL="$(downloadURLFromGit alienator88 Pearcleaner)"
+    appNewVersion="$(versionFromGit alienator88 Pearcleaner)"
+    expectedTeamID="BK8443AXLU"
+    ;;
